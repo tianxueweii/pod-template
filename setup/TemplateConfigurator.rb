d2ddf96a99cec4220482e96c2a4ff499e8df3cda
tianxuewei <382447269@qq.com>
2019-01-08 16:10:29 +0800

    Fix

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -70,21 +70,22 @@ module Pod
     def run
       @message_bank.welcome_message
 
-#      platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
+      # platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
 
-#      case platform
-#        when :macos
-#          ConfigureMacOSSwift.perform(configurator: self)
-#        when :ios
-#          framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
-#          case framework
-#            when :swift
-#              ConfigureSwift.perform(configurator: self)
-
-#            when :objc
-              ConfigureIOS.perform(configurator: self)
-#          end
-      end
+      # case platform
+      # when :macos
+      # ConfigureMacOSSwift.perform(configurator: self)
+      # when :ios
+      framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
+      case framework
+        when :swift
+          ConfigureSwift.perform(configurator: self)
+      
+        when :objc
+          ConfigureIOS.perform(configurator: self)
+          
+        end
+      # end
 
       replace_variables_in_files
       clean_template_files
